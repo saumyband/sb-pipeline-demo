c99decce01d0ed158bb45acc4d1aae1dd2d61467
Saumya Band <Saumya.Band@endava.com>
2026-03-20 14:34:27 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/glx/pipeline/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.glx.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String home() {
+        return "Hello, I am a Spring Boot Application!";
+    }
+}
---
+++ src/test/java/com/glx/pipeline/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glx.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
